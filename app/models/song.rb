7e4da51b626e30547d52b86906741ff74d72c1a2
nathan hains <nathanhains14@yahoo.com>
2021-04-14 11:20:06 -0400

    commit tree 0d438b225d35420600d561c6cfce19ff0debcf58

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,20 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def notes=(contents)
+      contents.each do |content|
+        self.notes.build(content: content).save
+      end
+  end
+
 end
